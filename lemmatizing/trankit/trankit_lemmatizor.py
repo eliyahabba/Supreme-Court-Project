7d1126e2b14a5deabc57306dd67b34ad15026d4e
noabitan12 <noabitan3@gmail.com>
2023-04-28 10:16:46 +0300

    changing trankit lemmatizor to work per sentence

--- lemmatizing/trankit/trankit_lemmatizor.py
+++ lemmatizing/trankit/trankit_lemmatizor.py
@@ -22,6 +22,24 @@ def create_trankit_pipeline(cache_dir):
                     cache_dir='C:/Users/noabi/PycharmProjects/cache/')
 
 
+def split_text(text):
+    """taking file raw text and splitting it to sentences,
+    returns 2-dimensional array containing splitted sentences"""
+    return [re.split(' |,|\t|\.|:|-', sen) for sen in text.split("\n")]
+
+
+def clean_text(splitted_text):
+    """cleaning text from . ? and other signs. returns a list of lists,
+    each list has a single element which is a word"""
+    clean_text = []
+    for sen in splitted_text:
+        clean_sen = [''.join(i for i in word if i.isalpha()) for word in sen]
+        clean_sen = [word for word in clean_sen if len(word) > 1]
+        if clean_sen:
+            clean_text.append(clean_sen)
+    return clean_text
+
+
 def run_trankit_file(pipeline, text):
     """processing a single file text and replacing words to be their lemma,
     returns a string"""
@@ -29,19 +47,15 @@ def run_trankit_file(pipeline, text):
     text_splitted = split_text(text)
     clean_text_splitted = clean_text(text_splitted)
     tokens = pipeline.lemmatize(clean_text_splitted)
-    tokens_df = pd.DataFrame.from_dict(
-        [token["tokens"][0] for token in tokens["sentences"]])
+    # tokens_df = pd.DataFrame.from_dict(
+    #     [token["tokens"][0] for token in tokens["sentences"]])
+    tokens = [[token.values() for token in sen] for sen in
+              [sen["tokens"] for sen in tokens["sentences"]]]
+    tokens = [[tuple(token) for token in sen] for sen in tokens]
+    tokens_df = pd.DataFrame([t for lst in tokens for t in lst]
+                             , columns=["index", "original", "lemma"])
+
     return " ".join(tokens_df["lemma"].values)
-
-
-def split_text(text):
-    """taking file raw text and splitting it to single words"""
-    return [[word] for word in re.split(' |\n|\t|\f|:|-|\.|,', text)]
-
-
-# def replace_special_words(text):
-#     """replacing words that are mapped on original text"""
-#     words_map = {'עו"ד': "עורך-דין"}
 
 
 def save_trankit_file(lemmatize_dir, filename, text, file_ind):
@@ -56,18 +70,6 @@ def save_trankit_file(lemmatize_dir, filename, text, file_ind):
     return
 
 
-def clean_text(splitted_text):
-    """cleaning text from . ? and other signs. returns a list of lists,
-    each list has a single element which is a word"""
-    clean_text = []
-    for lst in splitted_text:
-        word = lst[0]
-        clean_word = ''.join(i for i in word if i.isalpha())
-        if len(clean_word) > 1:
-            clean_text.append([clean_word])
-    return clean_text
-
-
 def run_trankit_pipeline(base_dir, text_dir, lemmatize_dir, cache_dir):
     pipeline = create_trankit_pipeline(cache_dir)
     done_text = list(os.listdir(lemmatize_dir))
@@ -75,8 +77,7 @@ def run_trankit_pipeline(base_dir, text_dir, lemmatize_dir, cache_dir):
     file_ind = 1
     for filename in os.listdir(text_dir):
         if filename in done_text:
-            #continue
-            pass
+            continue
         text_file = open(os.path.join(text_dir, filename), "r",
                          encoding='utf8')
         text = text_file.read()
