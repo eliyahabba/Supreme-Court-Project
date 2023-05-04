e9cf9d87db7938d1d59c4b0804855f475234b3d9
noabitan12 <noabitan3@gmail.com>
2023-05-04 17:37:19 +0300

    changes

--- lda/params_grid_search.py
+++ lda/params_grid_search.py
@@ -1,24 +1,29 @@
-from lda.lda import *
+from lda import *
 import itertools
 import numpy as np
 import datetime
+import os
+import pandas as pd
+import time
 
 
-def alpha_grid(min_alpha=0, max_alpha=1, step=0.1):
+def alpha_grid(min_alpha=0.0, max_alpha=1.0, step=0.1):
     return np.arange(min_alpha, max_alpha, step)
 
 
 def passes_grid(min_passes=5, max_passes=40, step=5):
-    return np.arrange(min_passes, max_passes, step)
+    return np.arange(min_passes, max_passes, step)
 
 
 def grid_params_lists(alpha_grid, passes_grid):
     return list(itertools.product(alpha_grid, passes_grid))
 
 
-def run_model_with_grid_search(base_dir, lemmatize_dir, results_dir,
-                               params_list):
+def run_model_with_grid_search(results_dir, docs,
+                               filenames_lst,
+                               corpus, dictionary, params_list):
     model_ind = 1
+    models_df = []
     for params in params_list:
         params_dict = {"num_topics": 15,
                        "chunksize": 2000,
@@ -29,16 +34,32 @@ def run_model_with_grid_search(base_dir, lemmatize_dir, results_dir,
                        }
         print(
             f"model num {model_ind} with params {params_dict} start: {datetime.datetime.now()}\n")
-        run_lda_pipeline(base_dir, lemmatize_dir, results_dir, params_dict)
+        bow, save_model_dir, score = run_lda_pipeline(results_dir, docs,
+                                                      filenames_lst,
+                                                      dictionary, corpus,
+                                                      params_dict)
+        params_dict["cohernce_score"] = score
+        models_df.append(params_dict)
         print(f"model num {model_ind} finished at: {datetime.datetime.now()}")
         model_ind += 1
+
+    pd.DataFrame(models_df).csv(
+        os.path.join(results_dir, f"{int(time.time())}_grid_search.csv"))
 
 
 if __name__ == "__main__":
     base_dir = r"C:\Users\noabi\PycharmProjects\University"
     lemmatize_dir = os.path.join(base_dir, r"LdaReadyText")
     results_dir = os.path.join(base_dir, r"YapLdaResults")
-    alphas = alpha_grid()
-    passes = passes_grid()
-    params = grid_params_lists(alphas, passes)
-    run_model_with_grid_search(base_dir, lemmatize_dir, results_dir, params)
+    alphas = alpha_grid(0.1, 0.3)
+    passes = passes_grid(5, 15)
+    params_list = grid_params_lists(alphas, passes)
+    print("creating docs")
+    docs, filenames_lst = create_docs(lemmatize_dir)
+    print("creating dictionary object")
+    dictionary = create_dictionary(docs, 300, 0.5)
+    print("creating bag of words")
+    corpus = create_bag_of_words(dictionary, docs)
+    print("running models")
+    run_model_with_grid_search(results_dir, docs,
+                               filenames_lst, corpus, dictionary, params_list)
